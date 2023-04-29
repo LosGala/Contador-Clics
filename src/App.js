0b65b01ec475d00bdc645222d959adae5e5fa325
LosGala <118622123+LosGala@users.noreply.github.com>
2023-04-29 11:23:03 -0300

    contador de clics

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import logoFreecodeCamp from "./imagenes/logoFreecodeCamp.png";
+import Boton from "./componentes/Boton";
+import Contador from "./componentes/Contador";
+import { useState } from "react";
+
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+
+  const manejarClic = () => {
+   setNumClics (numClics + 1) ;
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+    return (
+      <div className="App">
+        <div className="freecodecamp-logo-contenedor">
+          <img
+            className="freecodecamp-logo"
+            src={logoFreecodeCamp}
+            alt="logoFreecodeCamp"
+          />
+        </div>
+        <div className="contenedor-principal">
+          <Contador numClics={numClics} />
+          <Boton texto="clic" esBotonDeClic={true} manejarClic={manejarClic} />
+          <Boton
+            texto="Reiniciar"
+            esBotonDeClic={false}
+            manejarClic={reiniciarContador}
+          />
+        </div>
+      </div>
+    );
+}
+
+export default App;
